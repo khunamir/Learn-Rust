509bede99fa493d72dd39cca04dc5009dded9bd3
khunamir <amir12102009@gmail.com>
2021-04-29 14:50:09 +0800

    TRPL v1.1

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -11,21 +11,31 @@ fn main() {
 
 	println!("The secret number is {}", secret_number);
 
-    println!("Please enter your guess.");
+	loop {
+	    println!("Please enter your guess.");
 
-    let mut guess = String::new();
+	    let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-   			.expect("Failed to read the line");
+	    io::stdin().read_line(&mut guess)
+	   			.expect("Failed to read the line");
 
-	let guess: u32 = guess.trim().parse()
-			.expect("Please enter a number");
+		//let guess: u32 = guess.trim().parse()
+		//		.expect("Please enter a number");
 
-   	println!("You guessed: {}", guess);
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => continue,
+		};
 
-   	match guess.cmp(&secret_number) {
-   		Ordering::Less	=> println!("Too small!"),
-   		Ordering::Greater	=> println!("Too big!"),
-   		Ordering::Equal	=> println!("You Win!"),  
+	   	println!("You guessed: {}", guess);
+
+	   	match guess.cmp(&secret_number) {
+	   		Ordering::Less	=> println!("Too small!"),
+	   		Ordering::Greater	=> println!("Too big!"),
+	   		Ordering::Equal	=> {
+	   			println!("You Win!");
+	   			break;	
+	   		} 
+	   	}
    	}
 }
